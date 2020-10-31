b5c1fac95b12b95025420bdcaea178cf2a87874d
1 <a>
2020-10-31 15:10:16 +0800

    合作方，商铺入住

--- app/src/main/java/com/example/newpost/business_manger/Merchants_DatailActivity.java
+++ app/src/main/java/com/example/newpost/business_manger/Merchants_DatailActivity.java
@@ -1,15 +1,28 @@
 package com.example.newpost.business_manger;
 
+import android.annotation.SuppressLint;
 import android.graphics.Color;
+import android.os.Message;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.bigkoo.pickerview.builder.OptionsPickerBuilder;
+import com.bigkoo.pickerview.listener.OnOptionsSelectListener;
+import com.bigkoo.pickerview.view.OptionsPickerView;
 import com.example.newpost.R;
 import com.example.newpost.base.BaseActivity;
+import com.example.newpost.bean.JsonBean;
+import com.example.newpost.utils.GetJsonDataUtil;
+import com.google.gson.Gson;
+
+import org.json.JSONArray;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 作者: qgl
@@ -19,37 +32,22 @@ import com.example.newpost.base.BaseActivity;
 public class Merchants_DatailActivity extends BaseActivity implements View.OnClickListener {
     //返回键
     private ImageView iv_back;
-    // 商户名称
-    private EditText merchant_deteil_sname;
     //商户类型
     private TextView merchant_deteil_type;
     // 商户地址
     private TextView merchant_deteil_adress;
-    // 详细地址
-    private TextView merchant_deteils_adress_deta;
-    // 姓名
-    private EditText merchant_deteil_uname;
-    // 身份证号码
-    private EditText merchant_deteil_idcrad;
-    // 身份证有效期
-    private EditText merchant_deteil_card_year;
-    // 手机号
-    private EditText merchant_deteil_phone;
-    // 邮箱
-    private EditText merchant_deteil_email;
-    // 身份证正面
-    private ImageView merchant_deteil_cardis;
-    // 身份证反面
-    private ImageView merchant_deteil_cardthe;
-    // 店铺门头照
-    private ImageView merchant_deteil_dp1;
-    //店铺收银台照
-    private ImageView merchant_deteil_dp2;
-    //上传商铺内部照
-    private ImageView merchant_deteil_dp3;
     // 提交，保存，跳转
     private Button merchant_deteil_submint;
 
+
+    private static final int MSG_LOAD_DATA = 0x0001;
+    private static final int MSG_LOAD_SUCCESS = 0x0002;
+    private static final int MSG_LOAD_FAILED = 0x0003;
+    private List<JsonBean> options1Items = new ArrayList<>();
+    private ArrayList<ArrayList<String>> options2Items = new ArrayList<>();
+    private ArrayList<ArrayList<ArrayList<String>>> options3Items = new ArrayList<>();
+    private Thread thread;
+    private static boolean isLoaded = false;
     @Override
     protected int getLayoutId() {
         return R.layout.merchants_datailactivity;
@@ -60,9 +58,12 @@ public class Merchants_DatailActivity extends BaseActivity implements View.OnCli
         iv_back = findViewById(R.id.iv_back);
         merchant_deteil_submint = findViewById(R.id.merchant_deteil_submint);
         merchant_deteil_type = findViewById(R.id.merchant_deteil_type);
+        merchant_deteil_adress = findViewById(R.id.merchant_deteil_adress);
         iv_back.setOnClickListener(this);
         merchant_deteil_submint.setOnClickListener(this);
         merchant_deteil_type.setOnClickListener(this);
+        merchant_deteil_adress.setOnClickListener(this);
+        mHandler.sendEmptyMessage(MSG_LOAD_DATA);
 
     }
 
@@ -80,9 +81,148 @@ public class Merchants_DatailActivity extends BaseActivity implements View.OnCli
             case R.id.merchant_deteil_submint:
                 break;
             case R.id.merchant_deteil_type:
+
                 break;
             case R.id.merchant_deteil_adress:
+
+                if (isLoaded) {
+                    showPickerView();
+                } else {
+                    Toast.makeText(Merchants_DatailActivity.this, "Please waiting until the data is parsed", Toast.LENGTH_SHORT).show();
+                }
                 break;
         }
+    }
+
+
+    @SuppressLint("HandlerLeak")
+    private Handler mHandler = new Handler() {
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case MSG_LOAD_DATA:
+                    if (thread == null) {//如果已创建就不再重新创建子线程了
+
+                        Toast.makeText(Merchants_DatailActivity.this, "Begin Parse Data", Toast.LENGTH_SHORT).show();
+                        thread = new Thread(new Runnable() {
+                            @Override
+                            public void run() {
+                                // 子线程中解析省市区数据
+                                initJsonData();
+                            }
+                        });
+                        thread.start();
+                    }
+                    break;
+
+                case MSG_LOAD_SUCCESS:
+                    Toast.makeText(Merchants_DatailActivity.this, "Parse Succeed", Toast.LENGTH_SHORT).show();
+                    isLoaded = true;
+                    break;
+
+                case MSG_LOAD_FAILED:
+                    Toast.makeText(Merchants_DatailActivity.this, "Parse Failed", Toast.LENGTH_SHORT).show();
+                    break;
+            }
+        }
+    };
+
+    private void showPickerView() {// 弹出选择器
+
+        OptionsPickerView pvOptions = new OptionsPickerBuilder(this, new OnOptionsSelectListener() {
+            @Override
+            public void onOptionsSelect(int options1, int options2, int options3, View v) {
+                //返回的分别是三个级别的选中位置
+                String opt1tx = options1Items.size() > 0 ?
+                        options1Items.get(options1).getPickerViewText() : "";
+
+                String opt2tx = options2Items.size() > 0
+                        && options2Items.get(options1).size() > 0 ?
+                        options2Items.get(options1).get(options2) : "";
+
+                String opt3tx = options2Items.size() > 0
+                        && options3Items.get(options1).size() > 0
+                        && options3Items.get(options1).get(options2).size() > 0 ?
+                        options3Items.get(options1).get(options2).get(options3) : "";
+
+                String tx = opt1tx + opt2tx + opt3tx;
+//                Toast.makeText(Merchants_DatailActivity.this, tx, Toast.LENGTH_SHORT).show();
+                merchant_deteil_adress.setText(tx);
+            }
+        })
+                .setTitleText("城市选择")
+                .setDividerColor(Color.BLACK)
+                .setTextColorCenter(Color.BLACK) //设置选中项文字颜色
+                .setContentTextSize(20)
+                .build();
+
+        /*pvOptions.setPicker(options1Items);//一级选择器
+        pvOptions.setPicker(options1Items, options2Items);//二级选择器*/
+        pvOptions.setPicker(options1Items, options2Items, options3Items);//三级选择器
+        pvOptions.show();
+    }
+
+    private void initJsonData() {//解析数据
+        /**
+         * 注意：assets 目录下的Json文件仅供参考，实际使用可自行替换文件
+         * 关键逻辑在于循环体
+         *
+         * */
+        String JsonData = new GetJsonDataUtil().getJson(this, "province.json");//获取assets目录下的json文件数据
+        ArrayList<JsonBean> jsonBean = parseData(JsonData);//用Gson 转成实体
+        /**
+         * 添加省份数据
+         *
+         * 注意：如果是添加的JavaBean实体，则实体类需要实现 IPickerViewData 接口，
+         * PickerView会通过getPickerViewText方法获取字符串显示出来。
+         */
+        options1Items = jsonBean;
+        for (int i = 0; i < jsonBean.size(); i++) {//遍历省份
+            ArrayList<String> cityList = new ArrayList<>();//该省的城市列表（第二级）
+            ArrayList<ArrayList<String>> province_AreaList = new ArrayList<>();//该省的所有地区列表（第三极）
+
+            for (int c = 0; c < jsonBean.get(i).getCityList().size(); c++) {//遍历该省份的所有城市
+                String cityName = jsonBean.get(i).getCityList().get(c).getName();
+                cityList.add(cityName);//添加城市
+                ArrayList<String> city_AreaList = new ArrayList<>();//该城市的所有地区列表
+
+                //如果无地区数据，建议添加空字符串，防止数据为null 导致三个选项长度不匹配造成崩溃
+                /*if (jsonBean.get(i).getCityList().get(c).getArea() == null
+                        || jsonBean.get(i).getCityList().get(c).getArea().size() == 0) {
+                    city_AreaList.add("");
+                } else {
+                    city_AreaList.addAll(jsonBean.get(i).getCityList().get(c).getArea());
+                }*/
+                city_AreaList.addAll(jsonBean.get(i).getCityList().get(c).getArea());
+                province_AreaList.add(city_AreaList);//添加该省所有地区数据
+            }
+
+            /**
+             * 添加城市数据
+             */
+            options2Items.add(cityList);
+
+            /**
+             * 添加地区数据
+             */
+            options3Items.add(province_AreaList);
+        }
+        mHandler.sendEmptyMessage(MSG_LOAD_SUCCESS);
+
+    }
+
+    public ArrayList<JsonBean> parseData(String result) {//Gson 解析
+        ArrayList<JsonBean> detail = new ArrayList<>();
+        try {
+            JSONArray data = new JSONArray(result);
+            Gson gson = new Gson();
+            for (int i = 0; i < data.length(); i++) {
+                JsonBean entity = gson.fromJson(data.optJSONObject(i).toString(), JsonBean.class);
+                detail.add(entity);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            mHandler.sendEmptyMessage(MSG_LOAD_FAILED);
+        }
+        return detail;
     }
 }
--- app/src/main/java/com/example/newpost/fragment/Fragment2.java
+++ app/src/main/java/com/example/newpost/fragment/Fragment2.java
@@ -2,11 +2,8 @@ package com.example.newpost.fragment;
 
 import android.os.Bundle;
 import android.view.View;
-import android.view.WindowManager;
-
 import com.example.newpost.R;
 import com.example.newpost.base.BaseFragment;
-import com.gyf.barlibrary.ImmersionBar;
 
 
 /**
